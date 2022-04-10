b27d0ed77140e80ebe5eadb19ff7f206c305e084
magam54 <magma54@gmail.com>
2022-04-10 16:49:54 +0800

    debug

--- static/thankyou.js
+++ static/thankyou.js
@@ -13,62 +13,67 @@ recordbtn.addEventListener('click',function(){
     })
     .then(response=>response.json())
     .then(function(data){
-        attraction=data.data.trip.attraction.name
-        price=data.data.price
-        date=data.data.trip.date
-        time=data.data.trip.time
-        paystatus=data.data.status
-        let box = document.getElementById('thankyou')
-        let table=document.createElement('table')
-        box.appendChild(table)
-        let row1=document.createElement('tr')
-        let head1_1=document.createElement('th');
-        let head1_2=document.createElement('th');
-        head1_1.innerText="項目"
-        head1_2.innerText="內容"
-        row1.append(head1_1,head1_2)
-        let row2=document.createElement('tr')
-        let head2_1=document.createElement('th');
-        let head2_2=document.createElement('th');
-        head2_1.innerText="景點名稱"
-        head2_2.innerText=attraction
-        row2.append(head2_1,head2_2)
-        let row3=document.createElement('tr')
-        let head3_1=document.createElement('th');
-        let head3_2=document.createElement('th');
-        head3_1.innerText="訂單金額"
-        head3_2.innerText=price
-        row3.append(head3_1,head3_2)
-        let row4=document.createElement('tr')
-        let head4_1=document.createElement('th');
-        let head4_2=document.createElement('th');
-        head4_1.innerText="預定日期"
-        head4_2.innerText=date
-        row4.append(head4_1,head4_2)
-        let row5=document.createElement('tr')
-        let head5_1=document.createElement('th');
-        let head5_2=document.createElement('th');
-        head5_1.innerText="行程時間"
-        if (time=="morning"){
-            head5_2.innerText="早上9點到中午12點"
+        if (data.error==true){
+            document.getElementById('modal').style.visibility = "visible";
+            document.getElementById('modalcard_login').style.display = "block";
         }
-        if(time=="afternoon"){
-            head5_2.innerText="下午2點到下午5點"
+        if (data!=null){attraction=data.data.trip.attraction.name
+            price=data.data.price
+            date=data.data.trip.date
+            time=data.data.trip.time
+            paystatus=data.data.status
+            let box = document.getElementById('thankyou')
+            let table=document.createElement('table')
+            box.appendChild(table)
+            let row1=document.createElement('tr')
+            let head1_1=document.createElement('th');
+            let head1_2=document.createElement('th');
+            head1_1.innerText="項目"
+            head1_2.innerText="內容"
+            row1.append(head1_1,head1_2)
+            let row2=document.createElement('tr')
+            let head2_1=document.createElement('th');
+            let head2_2=document.createElement('th');
+            head2_1.innerText="景點名稱"
+            head2_2.innerText=attraction
+            row2.append(head2_1,head2_2)
+            let row3=document.createElement('tr')
+            let head3_1=document.createElement('th');
+            let head3_2=document.createElement('th');
+            head3_1.innerText="訂單金額"
+            head3_2.innerText=price
+            row3.append(head3_1,head3_2)
+            let row4=document.createElement('tr')
+            let head4_1=document.createElement('th');
+            let head4_2=document.createElement('th');
+            head4_1.innerText="預定日期"
+            head4_2.innerText=date
+            row4.append(head4_1,head4_2)
+            let row5=document.createElement('tr')
+            let head5_1=document.createElement('th');
+            let head5_2=document.createElement('th');
+            head5_1.innerText="行程時間"
+            if (time=="morning"){
+                head5_2.innerText="早上9點到中午12點"
+            }
+            if(time=="afternoon"){
+                head5_2.innerText="下午2點到下午5點"
+            }
+            row5.append(head5_1,head5_2)
+            let row6=document.createElement('tr')
+            let head6_1=document.createElement('th');
+            let head6_2=document.createElement('th');
+            head6_1.innerText="付款狀態"
+            if (paystatus==0){
+                head6_2.innerText="已付款"
+            }
+            if(paystatus==1){
+                head6_2.innerText="未付款"
+            }
+            row6.append(head6_1,head6_2)
+            table.append(row1,row2,row3,row4,row5,row6)
+            recordbtn.style.display="none"
         }
-        row5.append(head5_1,head5_2)
-        let row6=document.createElement('tr')
-        let head6_1=document.createElement('th');
-        let head6_2=document.createElement('th');
-        head6_1.innerText="付款狀態"
-        if (paystatus==0){
-            head6_2.innerText="已付款"
-        }
-        if(paystatus==1){
-            head6_2.innerText="未付款"
-        }
-        row6.append(head6_1,head6_2)
-        table.append(row1,row2,row3,row4,row5,row6)
-        recordbtn.style.display="none"
     })
 })
 
